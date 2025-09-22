d85750a6caf176ce009c107f4af1c52dc998c419
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 01:48:46 +0600

    discount section text

--- app/component/Discount.jsx
+++ app/component/Discount.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
+import { MdDone } from "react-icons/md";
+
 
 const Discount = () => {
   return (
@@ -27,6 +29,52 @@ const Discount = () => {
                         className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
                     Sofa Colletion</Link>
                  
+                </div>
+
+
+
+                {/* -------botom part------ */}
+
+                <div>
+
+
+
+{/* ---------text------- */}
+<div>
+
+<h2 className='text-primany text-[35px] font-medium font-adaminaaa'>20% Discount Of All Products</h2>
+<p className='text-[21px] font-adaminaaa font-medium text-pink'>Eams Sofa Compact</p>
+
+<p className='w-[523px] font-lato font-normal text-base text-[#B7BACB] mt-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu eget feugiat habitasse nec, bibendum condimentum.</p>
+
+<div className='mt-7 flex items-center gap-10'>
+    <div className='flex items-center gap-1'>
+        <MdDone className='text-xl text-[#7569B2]'/>
+
+        <p className='font-lato font-normal text-base text-[#B8B8DC] '>Material expose like metals</p>
+
+    </div>
+       <div className='flex items-center gap-1'>
+        <MdDone className='text-xl text-[#7569B2]'/>
+
+        <p className='font-lato font-normal text-base text-[#B8B8DC] '>Clear lines and geomatric figures</p>
+
+    </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
                 </div>
 
 
