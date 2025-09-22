878ec46b9bc325226c9bfc9dd97e03e4eb674bfb
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 00:59:16 +0600

    unique product section text st

--- app/component/UniqueProduct.jsx
+++ app/component/UniqueProduct.jsx
@@ -3,38 +3,46 @@ import Image from 'next/image'
 
 
 const UniqueProduct = () => {
-  return (
-    <>
+return (
+<>
 
     <section id='unique' className='mt-[135px] bg-[#F1F0FF] py-10'>
         <div className="container">
-            <div>
+            <div className='flex justify-around items-center'>
 
-             <div>
-                <Image
-                
-                src={"/Group 153.png"}
-                width={558}
-                height={550}
-                alt='Product'
-                
-                />
-             </div>
+                {/* ------image---------- */}
+                <div className=''>
+                    <Image src={"/Group 153.png"} width={558} height={550} alt='Product' />
+                </div>
+                {/* -=text------------- */}
+                <div>
+                    <h2 className='text-[#151875] text-[35px] font-medium font-adaminaaa w-[493px]'>
+                        Unique Features Of leatest &
+                        Trending Poducts
+                    </h2>
+                    <div className='flex gap-2 items-center mt-8'>
+                        <button className='w-2 h-2 rounded-full bg-red-500'></button>
+                        <p className='font-lato font-medium text-[#ACABC3] text-base'>All frames constructed with hardwood solids and laminates</p>
+                    </div>
+
+
+
+                </div>
             </div>
         </div>
     </section>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
+
+
+
+
+
+
+
+
+
+
+</>
+)
 }
 
 export default UniqueProduct
