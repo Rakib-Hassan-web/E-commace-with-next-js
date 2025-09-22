5df4d3409435968f9e9e9fdc5ab889268990591c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 19:43:05 +0600

    all cart s added need ta

--- app/component/FeaturedProducts.jsx
+++ app/component/FeaturedProducts.jsx
@@ -17,15 +17,15 @@ return (
 
 
 
-                <div className='w-[270px]   shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md'>
+                <div className='w-[270px]   shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md cursor-pointer'>
                     <div className='w-[270px]  bg-[#F6F7FB] flex justify-center items-center'>
 
 
-                        <Image className='w-[200px] ' src={'/image 1168.png'} width={100} height={100}
+                        <Image className='w-[200px] ' src={'/image 1168.png'} width={1j00} height={100}
                             alt='product image' />
 
                     </div>
-                    <div className='pb-5'>
+                    <div className='pb-5 hover:bg-[#2F1AC4] hover:text-white duration-300'>
 
                         <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato'>Cantilever chair</h2>
                         <div className='flex  justify-center items-center gap-2 mt-5'>
