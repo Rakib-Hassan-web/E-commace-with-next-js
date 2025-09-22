e90a493f73a52863d3c4bcb01879ef476198be4b
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 18:20:26 +0600

    banner paart text and button added

--- app/component/Banner.jsx
+++ app/component/Banner.jsx
@@ -1,17 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Banner = () => {
-  return (
-    <>
+return (
+<>
 
 
-    <section id='banner' className='bg-[#F2F0FF] pl-[236px] pt-[203px] w-full'>
+    <section id='banner' className='bg-[#F2F0FF] pb-[236px] pt-[203px] w-full'>
 
-      <div className="container">
-        <div className="div">
-            <h2 className=' te'> sdafsdf</h2>
+        <div className="container">
+            <div className="div">
+                {/* -----text---- */}
+                <h2 className='font-lato text-base font-bold text-pink'>Best Furniture For Your Castle....</h2>
+
+
+                <h1 className='mt-3  font-lato text-[54px] font-bold w-[644px]'>New Furniture Collection Trends In 2020
+                </h1>
+
+                <p className='font-lato font-bold text-base text-[#8A8FB9] w-[560px]  mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+                    in phasellus non in justo.</p>
+
+                    <div className='mt-[43px] '>
+                        <Link href={'#'} className='py-[15px] px-[43px] bg-pink rounded-[5px] text-white font-lato font-medium text-base  hover:bg-[#2F1AC4]  duration-300'>
+                        Shop Now
+                        </Link>
+                    </div>
+            </div>
         </div>
-      </div>
 
     </section>
 
@@ -20,8 +35,8 @@ const Banner = () => {
 
 
 
-    </>
-  )
+</>
+)
 }
 
 export default Banner
