42173984ffb41a752c9b318ff472e31e9e0d20a3
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 00:36:57 +0600

    offer section 1st cart

--- app/component/Offer.jsx
+++ app/component/Offer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Offer = () => {
   return (
@@ -8,6 +9,121 @@ const Offer = () => {
     
     
     
+    <section id='prodects' className='mt-[147px]  mb-1000 '>
+        <div className="container">
+            <div>
+                <h1 className=' font-aby font-normal text-[42px] text-[#1A0B5B] text-center'>What Shopex Offer!</h1>
+            </div>
+
+            {/* ------------all carts------- */}
+            <div className='flex items-center justify-between mt-15'>
+
+
+
+
+
+
+                <div className='w-[290px] pb-20 shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  '>
+                  
+                  <div className='flex justify-center mt-14'>
+                        <Image  
+                        src={'/free-delivery 1.png'}
+                        width={65}
+                        height={43}
+                        alt='logo'
+                        
+                        
+                        />
+                        </div>
+
+
+                    <h2 className='text-[22px] font-medium text-[#151875] text-center  font-adaminaaa mt-[27px] '>24/7 Support</h2>
+                   
+                    <p className='text-[16px] font-bold text-[#1a0b5b4e]  text-center  font-lato  px-6 mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
+                 
+
+
+                </div>
+
+
+
+   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
+                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
+
+
+
+                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
+                    <div className='flex  justify-center items-center gap-2 mt-4'>
+                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
+                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
+                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
+                    </div>
+                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
+                        Y523201</p>
+                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
+                    </p>
+
+
+                </div>
+
+
+
+
+
+   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
+                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
+
+
+
+                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
+                    <div className='flex  justify-center items-center gap-2 mt-4'>
+                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
+                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
+                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
+                    </div>
+                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
+                        Y523201</p>
+                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
+                    </p>
+
+
+                </div>
+
+
+
+
+   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
+                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
+
+
+
+                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
+                    <div className='flex  justify-center items-center gap-2 mt-4'>
+                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
+                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
+                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
+                    </div>
+                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
+                        Y523201</p>
+                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
+                    </p>
+
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    </section>
     
     
     
