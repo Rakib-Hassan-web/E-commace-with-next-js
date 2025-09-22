dfdf3a26f834f00bf26c3c29ac7bb251f43b0c50
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 16:51:36 +0600

    navbar work st

---
+++ app/component/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    
+    
+    <nav className='py-[19px] bg-red-400'>
+  <div className="container">
+    
+    
+  </div>
+    </nav>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Navbar
