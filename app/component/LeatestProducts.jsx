fb1e0c010cde963ea43d9111183a0e90bb6c592f
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 00:18:56 +0600

    leatest product component single cart

--- app/component/LeatestProducts.jsx
+++ app/component/LeatestProducts.jsx
@@ -1,5 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiSelfLove } from "react-icons/gi";
+
+
 
 const LeatestProducts = () => {
 return (
@@ -17,12 +21,68 @@ return (
 
 
                 <div className='flex items-center justify-center gap-18 mt-2'>
-                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300 '>New Arrival</Link>
-                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Best Seller</Link>
-                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Featured</Link>
-                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Special Offer</Link>
+                    <Link href={'#'}
+                        className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300 '>
+                    New Arrival</Link>
+                    <Link href={'#'}
+                        className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
+                    Best Seller</Link>
+                    <Link href={'#'}
+                        className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
+                    Featured</Link>
+                    <Link href={'#'}
+                        className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
+                    Special Offer</Link>
                 </div>
 
+                {/* ----------1st row of products------------ */}
+
+                <div className='mt-[62px]'>
+
+                    {/* ------siingle product----- */}
+                    <div className='relative group w-[360px] h-[306px]'>
+
+                        <img src="#" alt="Product Image" className='w-full h-[270px] bg-gray-100 rounded-md' />
+
+                        {/* hover cart icon */}
+                        <div
+                            className="absolute top-20 left-4    opacity-0 group-hover:opacity-100 transition duration-300">
+                            <button
+                                className="bg-white p-2 rounded-full shadow hover:bg-[#FB2448] hover:text-white duration-300">
+                                <AiOutlineShoppingCart />
+
+                            </button>
+                           
+                        </div>
+                          <div
+                            className="absolute top-30 left-4 opacity-0 group-hover:opacity-100 transition duration-300 ">
+                            <button
+                                className="bg-white p-2 rounded-full shadow hover:bg-[#FB2448] hover:text-white duration-300">
+                                <GiSelfLove />
+
+                            </button>
+                           
+                        </div>
+
+                        <div className=' mt-4 flex items-center justify-between '>
+                            <h2 className='  font-adaminaaa font-medium text-base text-[#151875]'>Comfort Handy Craft
+                            </h2>
+                            <div className='flex gap-3.5'>
+                                <p className=' font-adaminaaa font-bold text-[14px] text-[#151875]'>$42.00</p>
+                                <p className=' font-adaminaaa font-bold text-[12px] text-[#FB2448] line-through'> $65.00
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+                </div>
 
 
 
