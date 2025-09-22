1a09d52d7e6ed1fb474511a3fc51ccba833afb59
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 01:19:03 +0600

    discount section heading part complete

--- app/component/Discount.jsx
+++ app/component/Discount.jsx
@@ -13,22 +13,20 @@ const Discount = () => {
 
   {/* ------heading part-------- */}
 
-                <h1 className=' font-aby font-normal text-[42px] text-[#1A0B5B] text-center'>Leatest Products</h1>
+                <h1 className=' font-aby font-normal text-[42px] text-[#1A0B5B] text-center'>Discount Item</h1>
 
 
-                <div className='flex items-center justify-center gap-18 mt-2'>
+                <div className='flex items-center justify-center gap-12 mt-2'>
                     <Link href={'#'}
                         className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300 '>
-                    New Arrival</Link>
+                    Wood Chair</Link>
                     <Link href={'#'}
                         className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
-                    Best Seller</Link>
+                   Plastic Chair</Link>
                     <Link href={'#'}
                         className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
-                    Featured</Link>
-                    <Link href={'#'}
-                        className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>
-                    Special Offer</Link>
+                    Sofa Colletion</Link>
+                 
                 </div>
 
 
