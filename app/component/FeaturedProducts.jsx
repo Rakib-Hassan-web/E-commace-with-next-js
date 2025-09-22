8c9f5a4bf01b550d017f9ee7f37ce7da85a3f444
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 19:55:14 +0600

    single cart complet e

--- app/component/FeaturedProducts.jsx
+++ app/component/FeaturedProducts.jsx
@@ -20,20 +20,20 @@ return (
 
 
 
-                <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md '>
+                <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
                     <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
 
 
 
-                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato'>Cantilever chair</h2>
+                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
                     <div className='flex  justify-center items-center gap-2 mt-4'>
                         <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
                         <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
                         <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
                     </div>
-                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[10px] font-lato'>Code -
+                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
                         Y523201</p>
-                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato'>$42.00
+                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
                     </p>
 
 
@@ -50,82 +50,6 @@ return (
 
 
 
-
-
-                {/*
-
-
-
-                <div className='w-[270px]   shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md'>
-                    <div className='w-[270px]  bg-[#F6F7FB] flex justify-center items-center'>
-
-
-                        <Image className='w-[200px] ' src={'/image 1168.png'} width={100} height={100}
-                            alt='product image' />
-
-                    </div>
-                    <div className='pb-5'>
-
-                        <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato'>Cantilever chair</h2>
-                        <div className='flex  justify-center items-center gap-2 mt-5'>
-                            <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                            <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                            <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                        </div>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[15px] font-lato'>Code -
-                            Y523201</p>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato'>$42.00
-                        </p>
-                    </div>
-                </div>
-
-
-                <div className='w-[270px]   shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md'>
-                    <div className='w-[270px]  bg-[#F6F7FB] flex justify-center items-center'>
-
-
-                        <Image className='w-[200px] ' src={'/image 1168.png'} width={100} height={100}
-                            alt='product image' />
-
-                    </div>
-                    <div className='pb-5'>
-
-                        <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato'>Cantilever chair</h2>
-                        <div className='flex  justify-center items-center gap-2 mt-5'>
-                            <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                            <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                            <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                        </div>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[15px] font-lato'>Code -
-                            Y523201</p>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato'>$42.00
-                        </p>
-                    </div>
-                </div>
-
-
-                <div className='w-[270px]   shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md'>
-                    <div className='w-[270px]  bg-[#F6F7FB] flex justify-center items-center'>
-
-
-                        <Image className='w-[200px] ' src={'/image 1168.png'} width={100} height={100}
-                            alt='product image' />
-
-                    </div>
-                    <div className='pb-5'>
-
-                        <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato'>Cantilever chair</h2>
-                        <div className='flex  justify-center items-center gap-2 mt-5'>
-                            <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                            <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                            <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                        </div>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[15px] font-lato'>Code -
-                            Y523201</p>
-                        <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato'>$42.00
-                        </p>
-                    </div>
-                </div> */}
             </div>
         </div>
     </section>
