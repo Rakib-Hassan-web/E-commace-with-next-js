922a4cc28e1d31a499696cb5a395414827526701
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 17:18:19 +0600

    navbar logo and items

--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <>
-    
-    
-    <nav className='py-[19px] bg-red-400'>
-  <div className="container">
-    
-    
-  </div>
-    </nav>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
-}
+    <nav className="py-[20px] bg-white">
+      <div className="container  flex items-center justify-between  ">
+        
+        {/* ----- Logo ----- */}
+        <Link href="/" >
+          <Image
+            src="/Hekto.png"  
+            alt="Logo"
+            width={98}        
+           height={34}
+            priority           
+          />
+        </Link>
 
-export default Navbar
+        {/* -----  Items  ----- */}
+        <ul className="flex gap-8 ">
+          <li><Link href="/"  className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Home</Link></li>
+          <li><Link href="/about" className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Pages</Link></li>
+          <li><Link href="/shop" className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Products</Link></li>
+          <li><Link href="/contact" className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Blog</Link></li>
+          <li><Link href="/contact" className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Shop</Link></li>
+          <li><Link href="/contact" className="text-[#0D0E43] font-lato text-base font-normal hover:text-[#FB2E86] duration-300">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
