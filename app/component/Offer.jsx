6bd4ae4e1cd019da74461affd95fdb8fb827e13d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 00:39:09 +0600

    offer section cart 2

--- app/component/Offer.jsx
+++ app/component/Offer.jsx
@@ -22,7 +22,7 @@ const Offer = () => {
 
 
 
-
+                {/* ----------single cart---------- */}
                 <div className='w-[290px] pb-20 shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  '>
                   
                   <div className='flex justify-center mt-14'>
@@ -47,21 +47,50 @@ const Offer = () => {
 
 
 
-   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
-                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
+                   {/* ----------single cart---------- */}
+                <div className='w-[290px] pb-20 shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  '>
+                  
+                  <div className='flex justify-center mt-14'>
+                        <Image  
+                        src={'/cashback 1.png'}
+                        width={65}
+                        height={43}
+                        alt='logo'
+                        
+                        
+                        />
+                        </div>
+
+
+                    <h2 className='text-[22px] font-medium text-[#151875] text-center  font-adaminaaa mt-[27px] '>24/7 Support</h2>
+                   
+                    <p className='text-[16px] font-bold text-[#1a0b5b4e]  text-center  font-lato  px-6 mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
+                 
+
+
+                </div>
 
 
 
-                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
-                    <div className='flex  justify-center items-center gap-2 mt-4'>
-                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                    </div>
-                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
-                        Y523201</p>
-                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
-                    </p>
+                   {/* ----------single cart---------- */}
+                <div className='w-[290px] pb-20 shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  '>
+                  
+                  <div className='flex justify-center mt-14'>
+                        <Image  
+                        src={'/free-delivery 1.png'}
+                        width={65}
+                        height={43}
+                        alt='logo'
+                        
+                        
+                        />
+                        </div>
+
+
+                    <h2 className='text-[22px] font-medium text-[#151875] text-center  font-adaminaaa mt-[27px] '>24/7 Support</h2>
+                   
+                    <p className='text-[16px] font-bold text-[#1a0b5b4e]  text-center  font-lato  px-6 mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
+                 
 
 
                 </div>
@@ -69,50 +98,28 @@ const Offer = () => {
 
 
 
-
-   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
-                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
-
-
-
-                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
-                    <div className='flex  justify-center items-center gap-2 mt-4'>
-                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                    </div>
-                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
-                        Y523201</p>
-                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
-                    </p>
+                   {/* ----------single cart---------- */}
+                <div className='w-[290px] pb-20 shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  '>
+                  
+                  <div className='flex justify-center mt-14'>
+                        <Image  
+                        src={'/free-delivery 1.png'}
+                        width={65}
+                        height={43}
+                        alt='logo'
+                        
+                        
+                        />
+                        </div>
 
 
-                </div>
-
-
-
-
-   <div className='w-[270px] h-[351px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)] rounded-md  hover:bg-[#2F1AC4]  hover:text-white duration-300  group '>
-                    <img src="#" alt="product image" className='w-full h-[214px] bg-gray-100' />
-
-
-
-                    <h2 className='text-[18px] font-bold text-pink text-center mt-4 font-lato group-hover:text-white'>Cantilever chair</h2>
-                    <div className='flex  justify-center items-center gap-2 mt-4'>
-                        <button className='w-5 h-1.5 bg-red-500 rounded-4xl  cursor-pointer  '></button>
-                        <button className='w-5 h-1.5 bg-blue-500 rounded-4xl  cursor-pointer '></button>
-                        <button className='w-5 h-1.5 bg-green-500 rounded-4xl  cursor-pointer '></button>
-                    </div>
-                    <p className='text-[14px] font-semibold text-[#151875]  text-center mt-[10px] font-lato  group-hover:text-white'>Code -
-                        Y523201</p>
-                    <p className='text-[14px] font-semibold text-[#151875] text-center mt-[8px] font-lato  group-hover:text-white'>$42.00
-                    </p>
+                    <h2 className='text-[22px] font-medium text-[#151875] text-center  font-adaminaaa mt-[27px] '>24/7 Support</h2>
+                   
+                    <p className='text-[16px] font-bold text-[#1a0b5b4e]  text-center  font-lato  px-6 mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
+                 
 
 
                 </div>
-
-
-
 
 
 
