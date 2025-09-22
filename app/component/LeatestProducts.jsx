fc6d17c9b6657dd4e47800fa3c1e0a6ad7e81d09
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-22 23:53:23 +0600

    leatest product component heading done

--- app/component/LeatestProducts.jsx
+++ app/component/LeatestProducts.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const LeatestProducts = () => {
-  return (
-    <>
-    
+return (
+<>
+
     <section id='latestproduct' className='mt-[128px]'>
 
 
@@ -12,9 +13,15 @@ const LeatestProducts = () => {
 
                 {/* ------heading part-------- */}
 
-                              <h1 className=' font-aby font-normal text-[42px] text-[#1A0B5B] text-center'>Leatest Products</h1>
+                <h1 className=' font-aby font-normal text-[42px] text-[#1A0B5B] text-center'>Leatest Products</h1>
 
 
+                <div className='flex items-center justify-center gap-18 mt-2'>
+                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300 '>New Arrival</Link>
+                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Best Seller</Link>
+                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Featured</Link>
+                    <Link href={'#'} className='font-normal text-[18px] font-lato text-[#151875] hover:underline hover:text-pink duration-300'>Special Offer</Link>
+                </div>
 
 
 
@@ -25,13 +32,13 @@ const LeatestProducts = () => {
         </div>
 
     </section>
-    
-    
-    
-    
-    
-    </>
-  )
+
+
+
+
+
+</>
+)
 }
 
 export default LeatestProducts
