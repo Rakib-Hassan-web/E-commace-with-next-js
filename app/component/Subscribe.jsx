119fe7e160fdeab8c6be909b2bea33b07184eef0
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-23 10:03:27 +0600

    subscribe section add

--- app/component/Subscribe.jsx
+++ app/component/Subscribe.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Subscribe = () => {
   return (
     <>
     
+    <section id='subscribe' className=' relative'>
+
+        <div className=' absolute'>
+            <Image
+            
+            src={'/Group 162.png'}
+            width={1920}
+            height={462}
+            alt='bg'
+            
+            />
+
+        </div>
+<div className="container z-20">
+    <h2 className=''>Get Leatest Update By Subscribe
+0ur Newslater</h2>
+</div>
+    </section>
     
     
     
