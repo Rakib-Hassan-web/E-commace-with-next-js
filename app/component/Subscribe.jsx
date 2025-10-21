fa3693e0a9929859b6fcccaad545aa8bfa6432cf
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-21 23:28:52 +0600

    next js project home page  responsive complete

--- app/component/Subscribe.jsx
+++ app/component/Subscribe.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+
 const Subscribe = () => {
-return (
-<>
-
-  <section id='subscribe' className=' relative mt-[170px]'>
-
-    <div className=' absolute'>
-      <Image src={'/Group 162.png'} width={1920} height={462} alt='bg' />
-
-    </div>
-    <div className="container z-20 ">
-      <div className=' flex items-center justify-center text-center'>
-        <h2 className=' relative z-10  font-adaminaaa text-[35px] text-primany font-medium w-[563px] mt-[170px] '>Get
-          Leatest Update By Subscribe
-          0ur Newslater</h2>
+  return (
+    <section id='subscribe' className='relative py-40 sm:py-32'>
+      {/* Background Image */}
+      <div className='absolute inset-0 -z-10'>
+        <Image
+          src='/Group 162.png'
+          alt='bg'
+          fill
+          className='object-cover w-full h-full'
+          priority
+        />
       </div>
-            <div className='mt-[28px] relative z-10 flex items-center justify-center text-center'>
-                    <Link href={'#'}
-                        className='py-[15px] px-[43px] bg-pink rounded-[5px] text-white font-lato font-medium text-base hover:bg-[#2F1AC4] duration-300'>
-                    Shop Now
-                    </Link>
-                </div>
 
-    </div>
-  </section>
+      <div className='container mx-auto px-4 flex flex-col items-center justify-center text-center'>
+        {/* Heading */}
+        <h2 className='font-adaminaaa font-medium text-2xl sm:text-3xl md:text-4xl text-primany max-w-xl sm:max-w-2xl'>
+          Get Latest Update By Subscribe Our Newsletter
+        </h2>
 
-
-
-
-
-
-</>
-)
+        {/* Button */}
+        <div className='mt-7'>
+          <Link
+            href='#'
+            className='py-3 px-8 sm:py-4 sm:px-12 bg-pink rounded-md text-white font-lato font-medium text-base sm:text-lg hover:bg-[#2F1AC4] transition-colors duration-300'
+          >
+            Shop Now
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Subscribe
